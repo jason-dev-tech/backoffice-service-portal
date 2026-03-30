4a70d60ebe42ecd24c0f292029b162ddf8a7d30a
Jason <>
2026-03-30 22:33:57 +1030

    Add ApplicationRoles constants for RBAC

---
+++ backend/BackofficeServicePortal.Api/Models/ApplicationRoles.cs
@@ -0,0 +1,8 @@
+namespace BackofficeServicePortal.Api.Models;
+
+public static class ApplicationRoles
+{
+    public const string Admin = "Admin";
+    public const string Operator = "Operator";
+    public const string Viewer = "Viewer";
+}

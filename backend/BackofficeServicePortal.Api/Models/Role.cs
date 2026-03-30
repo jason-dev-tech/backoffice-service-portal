6b1634ea7da65b334e4d8e6e8f11b1641b35e17f
Jason <>
2026-03-30 18:10:27 +1030

    Add user navigation to Role model

--- backend/BackofficeServicePortal.Api/Models/Role.cs
+++ backend/BackofficeServicePortal.Api/Models/Role.cs
@@ -5,4 +5,6 @@ public class Role
     public int Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
+
+    public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
 }

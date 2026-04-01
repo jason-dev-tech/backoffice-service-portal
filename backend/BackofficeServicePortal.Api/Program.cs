538d68fb9361da5fac577bbed40baed4d519fd32
Jason <>
2026-04-01 17:28:22 +1030

    Register password hasher service in dependency injection

--- backend/BackofficeServicePortal.Api/Program.cs
+++ backend/BackofficeServicePortal.Api/Program.cs
@@ -66,6 +66,7 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddSingleton<ServiceRequestAuditLogService>();
 builder.Services.AddScoped<IServiceRequestService, ServiceRequestService>();
+builder.Services.AddScoped<IPasswordHasherService, PasswordHasherService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));

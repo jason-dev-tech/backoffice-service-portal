c8465a09b229cfe292449c58841b12733fc88e27
Jason <>
2026-04-03 01:17:37 +1030

    feat(routing): add service requests page and initial routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ServiceRequestsPageComponent } from './pages/service-requests/service-requests-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: ServiceRequestsPageComponent },
+];
